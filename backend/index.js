af9953504cd07e9c4d2893e6e40c770f117d309a
sanjanaDinith <sanjana.dinith.r@gmail.com>
2023-10-14 10:15:43 +0530

    Index routes updated

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 
+// Import route files for each collection
 const supplierRoutes = require('./routes/supplier.routes');
+const orderRoutes = require('./routes/orderTest.routes');
+const procurementOfficerRoutes = require('./routes/procumentOfficer.routes');
+const requestOrderRoutes = require('./routes/requestOrder.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,8 +16,13 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 app.use(bodyParser.json());
+
+// Use the routes for each collection with appropriate base paths
 app.use('/supplier', supplierRoutes);
+app.use('/orders', orderRoutes);
+app.use('/procurement-officers', procurementOfficerRoutes);
+app.use('/request-orders', requestOrderRoutes);
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
